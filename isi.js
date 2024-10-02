13fc06a1f23c4e7382ed456e5ca6d3d0b4aaa6b7
liy0fstar <mauliyaindah243@gmail.com>
2024-10-02 15:06:25 +0700

    Update isi.js

--- isi.js
+++ isi.js
@@ -4,4 +4,4 @@ document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
 document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
 document.writeln("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h3>");
 document.writeln("<hr width=600 color=Black>");
-doument.writeln("</center>");
+document.writeln("</center>");
